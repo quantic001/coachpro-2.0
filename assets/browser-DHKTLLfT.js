86ba8d648bf03c8da07d0f033636140e2fbc33ba
quantic001 <quantic001@users.noreply.github.com>
2026-09-20 11:57:24 -0400

    Deploy CoachPro 2.0 demo

---
+++ assets/browser-DHKTLLfT.js
@@ -0,0 +1,1 @@
+import{t as e}from"./rolldown-runtime-B0Z9INg1.js";var t=e(((e,t)=>{t.exports=function(){throw Error(`ws does not work in the browser. Browser clients must use the native WebSocket object`)}}));export default t();
